b6988cbcc6c6f3fe42585fbce4b308638b475338
AdamLuczaj <adamluczaj123@gmail.com>
2023-11-23 15:48:04 -0500

    Created HelpStage

---
+++ src/aed-sim/helpStage.cpp
@@ -0,0 +1,8 @@
+#include "helpStage.h"
+
+HelpStage::HelpStage() {
+    displayText = "CALL FOR HELP.";
+    orderInSequence = 1;
+}
+
+HelpStage::~HelpStage() { }
---
+++ src/aed-sim/helpStage.h
@@ -0,0 +1,12 @@
+#ifndef HELPSTAGE_H
+#define HELPSTAGE_H
+
+#include "aedStage.h"
+
+class HelpStage : public AEDStage {
+    public:
+        HelpStage();
+        ~HelpStage();
+};
+
+#endif
