f826731be0e9e7fadbe93e48bca1bb4a52b6d6a0
AdamLuczaj <adamluczaj123@gmail.com>
2023-11-27 17:56:45 -0500

    initial commit

--- src/aed-sim/cprStage.cpp
+++ src/aed-sim/cprStage.cpp
@@ -10,4 +10,19 @@ CprStage::~CprStage() { }
 void CprStage::start() {
     updateDisplay(displayText);
     QThread::sleep(1);
+
+    qDebug() << "User begins chest compressions on victim.";
+    QThread::sleep(5);
+
+    displayText = "CONTINUE CPR.";
+    updateDisplay(displayText);
+
+    QThread::sleep(115); //User completes 2 minutes CPR.
+
+    //ASK: do we want to check for good compressions or not? (2 inches deep) - would involve displaying GOOD COMPRESSIONS or BAD COMPRESSIONS
+
+    displayText = "STOP CPR.";
+    updateDisplay(displayText);
+    qDebug() << "User stops doing chest compressions on victim.";
+
 }
