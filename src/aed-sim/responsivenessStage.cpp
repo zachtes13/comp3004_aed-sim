c3b4986fd305acb45e46d5ae39cd9fc1481e628e
VinceCardillo <Vince.Cardillo@hotmail.com>
2023-12-03 01:02:46 -0500

    removed a comment

--- src/aed-sim/responsivenessStage.cpp
+++ src/aed-sim/responsivenessStage.cpp
@@ -8,7 +8,6 @@ ResponsivenessStage::ResponsivenessStage() {
 ResponsivenessStage::~ResponsivenessStage() { }
 
 void ResponsivenessStage::start() {
-//    updateDisplay(displayText);
     QThread::sleep(2);
     qDebug() << "User checks victim's responsiveness.";
     QThread::sleep(2);
